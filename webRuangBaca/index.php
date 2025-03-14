b0be80c7cc79159cd1f2b14a1980350120a9dc13
prayogi270201 <135359533+prayogi270201@users.noreply.github.com>
2025-03-14 22:26:01 +0700

    jangar

--- webRuangBaca/index.php
+++ webRuangBaca/index.php
@@ -105,7 +105,7 @@ Admin Of Library Management System";
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
     rel="stylesheet" />
-  <!-- Fontawesome Link for Icons -->
+  <!-- link jangar sekali -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
 </head>
 
